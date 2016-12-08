f31827316931654413e6397ef220c2bfcc23ffec
Emil Lengman <emillen@kth.se>
2016-12-08 12:10:43 +0100

    getters and setters

--- src/java/currencyConverter/controller/CurrencyConverter.java
+++ src/java/currencyConverter/controller/CurrencyConverter.java
@@ -7,7 +7,8 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 /**
- * This controller type class will be doing the currency converting 
+ * This controller type class will be doing the currency converting
+ *
  * @author daseel
  */
 @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
@@ -16,5 +17,32 @@ public class CurrencyConverter {
 
     @PersistenceContext(unitName = "CurrencyConverterPU")
     private EntityManager em;
-    
+
+    private String to;
+    private String from;
+    private float amount;
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
 }
